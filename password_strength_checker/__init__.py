388312aebb30aec174af6a5841f31d59ff91927f
maram drira <maramdrira13@gmail.com>
2025-07-24 19:39:16 +0100

    Deploy to AzureT

--- password_strength_checker/__init__.py
+++ password_strength_checker/__init__.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)  # This must be named 'app' unless you configure otherwise
+from .app import app, check_strength
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()  # Only for local development
+__all__ = ['app', 'check_strength']
